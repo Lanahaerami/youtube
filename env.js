a7d71db6b351eb2e553832d681da43aa7cebf2db
Lanahaerami <71107183+Lanahaerami@users.noreply.github.com>
2020-09-21 16:19:23 +0300

    Update env.js

--- env.js
+++ env.js
@@ -6,9 +6,9 @@
 # reference these in your code with process.env.SECRET
 
 PREFIX=-
-BOT_TOKEN=NzI2NjU5OTM4NjkyMjM1MzQ0.XwNmfA.aKWQisLEzZiKpZkXyI5SSwIU95s
+BOT_TOKEN=NzI0MzkxOTExOTE3NDg2MTAw.X2innQ.rwfdR9ZqZOf2L2JlihJDSfQEyos", "user_settings": {"locale": "en-US", "theme": "dark
 YTAPI_KEY=AIzaSyAv9bQOZON0kF-OKmrKMQE40ulHfiPVD7s
-PROJECT_DOMAIN=mufobot
+PROJECT_DOMAIN=youtube
 
 
 # note: .env is a shell file so there can't be spaces around =
